c019e8637e61b2e5d14e8c16e61cf5d74024076e
imagineEven <1016000928@qq.com>
2018-10-25 12:34:42 +0800

    class extends phaser.game over

--- src/utils/phaser.js
+++ src/utils/phaser.js
@@ -1,25 +1,17 @@
 window.PIXI = require('phaser-ce/build/custom/pixi.js');
 window.p2 = require('phaser-ce/build/custom/p2.js');
 window.Phaser = require('phaser-ce/build/custom/phaser-split.js');
-
-// this.game = new Phaser.Game('100', '100', Phaser.AUTO, this.$refs.gameScreen, {
-//   preload: rotate.preload,
-//   create: rotate.create,
-//   update: rotate.update
-// })
-
-class BasePhaser extends Phaser.Game {
+import vuePng from '@/assets/logo.png';
+//, preload, create, update
+class BasePhaser{
   constructor(view, preload, create, update) {
-    console.log('BasePhaser')
-    super('100', '100', Phaser.AUTO, view, {
-      preload: preload,
-      create: create,
-      update: update
-    })
-  }
-
-  nihao() {
-
+    if (this.game == null) {
+      this.game = new Phaser.Game('100', '100', Phaser.AUTO, view, {
+        preload,
+        create,
+        update
+      })
+    }
   }
 }
 
--- src/views/phaser/mixin/index.js
+++ src/views/phaser/mixin/index.js
@@ -1,37 +1,26 @@
-window.PIXI = require('phaser-ce/build/custom/pixi.js');
-window.p2 = require('phaser-ce/build/custom/p2.js');
-window.Phaser = require('phaser-ce/build/custom/phaser-split.js');
 import vuePng from '@/assets/logo.png'
-import { Rotate } from './rotate'
-
+import { RotateGame } from './rotateGame'
 export default {
 
   mounted() {
-    new Rotate(this.$refs.gameScreen)
-    // if (this.game == null) {
-    //   this.game = new Phaser.Game('100', '100', Phaser.AUTO, this.$refs.gameScreen, {
-    //     preload: rotate.preload,
-    //     create: rotate.create,
-    //     update: rotate.update
-    //   })
-    // }
+    new RotateGame(this.$refs.gameScreen)
   },
   methods: {
-    // preload (game) {
-    //   this.game.load.image('sprite', vuePng)
-    // },
-    // create (game) {
+    preload () {
+      this.game.load.image('sprite', vuePng)
+    },
+    create () {
 
-    //   let vueImage = this.game.add.sprite(100, 100, 'sprite')
-    //   vueImage.inputEnabled = true
-    //   vueImage.input.enableDrag()
-    //   vueImage.events.onInputDown.add(() => {
-    //     console.log('图片被点击了')
-    //   })
-    //   //game.add.tween(sprite).to( { y: game.world.centerY }, 4000, Phaser.Easing.Bounce.Out, true);
-    // },
-    // update (game) {
+      let vueImage = this.game.add.sprite(100, 100, 'sprite')
+      vueImage.inputEnabled = true
+      vueImage.input.enableDrag()
+      vueImage.events.onInputDown.add(() => {
+        console.log('图片被点击了')
+      })
+      //game.add.tween(sprite).to( { y: game.world.centerY }, 4000, Phaser.Easing.Bounce.Out, true);
+    },
+    update () {
 
-    // },
+    },
   }
 }
