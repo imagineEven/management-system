f424365f46cc6595f6f6f7ecb544413ba8d9f237
imagineEven <1016000928@qq.com>
2018-10-25 13:13:19 +0800

    nihao

--- src/views/phaser/mixin/index.js
+++ src/views/phaser/mixin/index.js
@@ -3,24 +3,6 @@ import { RotateGame } from './rotateGame'
 export default {
 
   mounted() {
-    new RotateGame(this.$refs.gameScreen)
-  },
-  methods: {
-    preload () {
-      this.game.load.image('sprite', vuePng)
-    },
-    create () {
-
-      let vueImage = this.game.add.sprite(100, 100, 'sprite')
-      vueImage.inputEnabled = true
-      vueImage.input.enableDrag()
-      vueImage.events.onInputDown.add(() => {
-        console.log('图片被点击了')
-      })
-      //game.add.tween(sprite).to( { y: game.world.centerY }, 4000, Phaser.Easing.Bounce.Out, true);
-    },
-    update () {
-
-    },
+    let rotateGame = new RotateGame(this.$refs.gameScreen)
   }
 }
